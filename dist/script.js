e781a86efebaa01ad376b565ae056a50650477f1
Chi Derah <drexydan@gmail.com>
2022-09-16 11:31:22 +0100

    I removed navbar-cng for this site

--- dist/script.js
+++ dist/script.js
@@ -1,12 +1,12 @@
-let navbarDiv = document.querySelector('nav');
-window.addEventListener('scroll', () => {
-    if(document.body.scrollTop > 40 
-    || document.documentElement.scrollTop > 40){
-        navbarDiv.classList.add('navbar-cng');
-    } else {
-        navbarDiv.classList.remove('navbar-cng')
-    }
-});
+// let navbarDiv = document.querySelector('nav');
+// window.addEventListener('scroll', () => {
+//     if(document.body.scrollTop > 40 
+//     || document.documentElement.scrollTop > 40){
+//         navbarDiv.classList.add('navbar-cng');
+//     } else {
+//         navbarDiv.classList.remove('navbar-cng')
+//     }
+// });
 
 
 
